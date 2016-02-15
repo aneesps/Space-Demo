46e36451993f0bd879707fb29f61a77dd24ea5a3
hawariotm <hawariotm@gmail.com>
2016-02-15 19:11:08 +0530

    implemented profile view funcionality

--- controllers/profilecontroller.js
+++ controllers/profilecontroller.js
@@ -3,7 +3,13 @@
 
 
   var app = angular.module('myApp', []);
-            app.controller('customersCtrl', function($scope, $http) {
+             
+            app.controller('profileCtrl', function($scope, $http) {
+            	$scope.arra=[];
             $http.get("../assets/json/employee.json")
-            .success(function (data) {$scope.names =data;});
+            .success(function (data) {$scope.names =data;
+   
+            	$scope.arra = data[0].basic.split(",");
+            
+            });
             });
