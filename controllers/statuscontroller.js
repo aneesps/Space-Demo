bb9d872ce2dc301075073d2672218d3baca78c18
anees <anees@qburst.com>
2016-02-17 19:11:28 +0530

    Completed the functionality of Daily status

--- controllers/statuscontroller.js
+++ controllers/statuscontroller.js
@@ -1,20 +1,58 @@
-statusApp = angular.module("statusApp", []);
-statusApp.controller("statusController", function($scope) {
+// statusApp = angular.module("statusApp", []);
+mainApp.controller("statusController", function($scope) {
     
 		$scope.statuses=[];
-		$scope.dates = [];
+		$scope.hoursList=[];
+		for(i=1;i<=24;i++){
+			$scope.hoursList.push({
+				value:i,
+				hour:i
+			});
+		}
+		$scope.minutesList=[];
+		for(i=0;i<4;i++){
+			$scope.minutesList.push({
+				value:i,
+				minute:i*15
+			});
+		}
+		$scope.dateValues = [{
+			'date':'',
+			'value':''
+		},{
+			'date':'',
+			'value':''
+		},{
+			'date':'',
+			'value':''
+		},{
+			'date':'',
+			'value':''
+		},{
+			'date':'',
+			'value':''
+		},{
+			'date':'',
+			'value':''
+		},{
+			'date':'',
+			'value':''
+		}];
+		$scope.activities=['training','coding','testing','debugging'];
 		var d = new Date();
-		for(i=7,j=7;i>=0;i--,j--)
+		for(i=0,j=0;i<7;i++,j++)
 		{
-				$scope.dates[j]=d.setDate(d.getDate()-1);
-				console.log($scope.dates);
-				console.log(d);
+				$scope.dateValues[j].date=d.setDate(d.getDate()-1);
+				$scope.dateValues[j].value=i;
 		};
 		
         $scope.submit = function() {
 			$scope.statuses.push({
-								'activity':$scope.activity,
+								'date':$scope.dateValues[$scope.date].date,
+								'activity':$scope.activities[$scope.activity],
 								'project':$scope.project,
+								'hours':$scope.hoursList[$scope.hours-1].hour,
+								'minutes':$scope.minutesList[$scope.minutes].minute,
 								'report':$scope.report
 							});
         };
