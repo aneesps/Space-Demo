9969cd4ebf46e26ad643626756e750bc89054a85
anees <anees@qburst.com>
2016-02-18 11:26:25 +0530

    styling the history table

--- controllers/statuscontroller.js
+++ controllers/statuscontroller.js
@@ -1,6 +1,5 @@
 // statusApp = angular.module("statusApp", []);
 mainApp.controller("statusController", function($scope) {
-    
 		$scope.statuses=[];
 		$scope.hoursList=[];
 		for(i=1;i<=24;i++){
@@ -40,21 +39,25 @@ mainApp.controller("statusController", function($scope) {
 		}];
 		$scope.activities=['training','coding','testing','debugging'];
 		var d = new Date();
+		$scope.today=d;
 		for(i=0,j=0;i<7;i++,j++)
 		{
 				$scope.dateValues[j].date=d.setDate(d.getDate()-1);
 				$scope.dateValues[j].value=i;
 		};
 		
+		
         $scope.submit = function() {
 			$scope.statuses.push({
 								'date':$scope.dateValues[$scope.date].date,
+								'todayDate':$scope.today,
 								'activity':$scope.activities[$scope.activity],
 								'project':$scope.project,
 								'hours':$scope.hoursList[$scope.hours-1].hour,
 								'minutes':$scope.minutesList[$scope.minutes].minute,
 								'report':$scope.report
 							});
+			
         };
 
     
