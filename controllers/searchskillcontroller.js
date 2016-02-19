88878f66935da59c21a8bb0f074bb9ee1310f440
Akshayakt <akshaya@qburst.com>
2016-02-19 17:57:08 +0530

    completed functionality of skillsearch

--- controllers/searchskillcontroller.js
+++ controllers/searchskillcontroller.js
@@ -1,10 +1,18 @@
 
 var app= angular.module("skillApp",[]);
-app.controller("searchskillcontroller",function($scope,$http){
+app.controller('searchskillcontroller',function($scope,$http,$filter){
 	$scope.skillsData = [];
-    $http.get("../assets/json/skills.json").success(function(data) {
-      	 $scope.skillsData=data;
-      	 
+	$http.get("../assets/json/skills.json").success(function(data) {
+       $scope.skillsData=data;
       });
-});
+    $scope.searchSkill=function(){
+    	    if($scope.keyword == undefined){
+    	    	return true;}
+            $scope.results = $filter('filter')($scope.skillsData, {skills:$scope.keyword} ,function(actual, expected) { 
+            	actual=$filter('uppercase')(actual);
+            	expected=$filter('uppercase')(expected);
+            	return angular.equals(actual, expected)});	
+   } ;
 
+ 	
+});
